fb82b35521a8bb0e6504d628f5067fb2a25a5c59
MaurizioB <maurizio.berti@gmail.com>
2017-12-25 18:05:46 +0100

    Version change to 0.8.1

--- samplebrowsesrc/info.py
+++ samplebrowsesrc/info.py
@@ -1,6 +1,6 @@
 MAJ_VERSION = 0
-MIN_VERSION = 7
-REV_VERSION = 0
+MIN_VERSION = 8
+REV_VERSION = 1
 __author__ = 'Maurizio Berti'
 __version__ = '{}.{}.{}'.format(MAJ_VERSION, MIN_VERSION, REV_VERSION)
 __codeurl__ = __website__ = 'https://github.com/MaurizioB/SampleBrowse'
